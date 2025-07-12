5c89eaed1d99f17b83258a80d0086201b55f6c03
uriel <diprima.ale@gmail.com>
2025-07-12 19:03:46 +0200

    Cambia layout e aggiungi colori

--- script.js
+++ script.js
@@ -1,96 +1,96 @@
 const staticLink = "https://www.paypal.com/cgi-bin/webscr?cmd=_xclick&business=diprima.ale@gmail.com&currency_code=EUR&amount=%amount%&item_name=%title%";
 const isMobile = window.innerWidth <= 768
 
-const experiences = {
-    "Akihabara-AtHome": {
+const experiences = [
+    {
         title: "Colazione/Merenda At Home Cafe",
         description: 'Colazione o merenda in un maid cafe, costretti a ballare e cantare motivetti stupidi pur di mangiare qualcosa...che ci piaccia o no.',
-        amount: "20",
+        amount: 20,
         image: "img/At-home_cafe.jpg",
         city: "Akihabara",
         position: [35.69964414574883, 139.77046106942902]
     },
-    "Ueno-Swans": {
+     {
         title: "Cigni a Ueno",
         description: "Una romantica ed imbarazzante nuotata nel parco di Ueno (Tokyo). Ci si guadagna in amore ma si perde in dignità",
-        amount: "10",
+        amount: 10,
         image: "img/swanboat-ueno.jpg",
         city: "Ueno",
         position: [35.71199849138613, 139.77191305357505]
     },
-    "shinkansen-tokyo-hiroshima": {
+     {
         title: "Shinkansen Tokyo-Hiroshima",
         description: "Viaggio nel famoso treno proiettile, partenza Tokyo destinazione Hiroshima...ci vorrà un pò",
-        amount: "200",
+        amount: 200,
         image: "img/shinkansen-tokyo-hiroshima.jpg",
         city: "Hiroshima",
         position: [35.12499575494723, 138.48630358663596]
     },
-    "okonomimura": {
+     {
         title: "Okonomiyaki a Okonomimura",
         description: 'Non spaventatevi per il titolo, andiamo solo a provare la famosa Okonomiyaki (tipo una frittata) di Hiroshima.',
-        amount: "20",
+        amount: 20,
         image: "img/okonomimura.jpg",
         city: "Hiroshima",
         position: [34.39155812061836, 132.4618584820796]
     },
-    "traghetto-hiroshima-miyajima": {
+     {
         title: "Traghetto per l'isola di Miyajima",
         description: 'Traghetto per raggiungere Miyajima dove potremo ammirare il famoso Torii (portale) immerso nell\'acqua.',
-        amount: "20",
+        amount: 20,
         image: "img/traghetto-hiroshima-miyajima.jpg",
         city: "Miyajima",
         position: [34.302495052637944, 132.32239497358432]
     },
-    "rilakkuma-sanbo": {
+     {
         title: "Pranzo al Rilakkuma Sabo",
         description: 'Offriteci un pranzo nel ristorante di una famosa mascotte giapponese che Sara adora... probabilmente impazzirà',
-        amount: "50",
+        amount: 50,
         image: "img/rilakkuma-sabo.jpg",
         city: "Miyajima",
         position: [34.30940248285368, 132.31291859843256]
     },
-    "shinkansen-hiroshima-himeji": {
+     {
         title: "Shinkansen Hiroshima-Himeji",
         description: 'Ci spostiamo di nuovo con il treno proiettile verso Himeji, la città del famoso castello dell\'airone bianco',
-        amount: "100",
+        amount: 100,
         image: "img/shinkansen-hiroshima-himeji.jpg",
         city: "Himeji",
         position: [34.71342851918433, 134.14739062554887]
     },
-    "himeji-castle": {
+     {
         title: "Castello di Himeji",
         description: 'Visita al castello di Himeji, patrimonio dell\'UNESCO',
-        amount: "10",
+        amount: 10,
         image: "img/himeji-castle.jpg",
         city: "Himeji",
         position: [34.83951941878888, 134.69389396859842]
     },
-    "Fukusaki-Yokai": {
+     {
         title: "Tour Yokai in bici",
         description: 'Tour in bici per la cittadina di Fukusaki dove in ogni angolo si nascondono yokai e mostri giapponesi. Vediamo se riusciamo a trovarli tutti.',
-        amount: "10",
+        amount: 10,
         image: "img/fukusaki-yokai.jpg",
         city: "Fukusaki",
         position: [34.95413939702594, 134.75666483282498]
     },
-    "nipponia": {
+     {
         title: "Soggiorno a Nipponia",
         description: 'Vitto, alloggio e terme in un albergo tradizionale nella piccola cittadina di Fukusaki. Con tanto di vasca termale compresa nella stanza <3',
-        amount: "300",
+        amount: 300,
         image: "img/nipponia.jpg",
         city: "Fukusaki",
         position: [34.9562003379001, 134.76425975510884]
     },
-};
+];
 
-
+experiences.sort((x,y) => (x.amount - y.amount));
 
 function BuildPopupContent(title, image, description){
-    return '<div style="max-height: 30vh; overflow: hidden; display: flex; flex-direction: column; align-items: center; justify-content: center;">'+
-    '<h4 style="margin:0 0 8px 0;color:#008bf8">' + title + '</h4>'+
+    return '<div style="max-height: 40vh; overflow: hidden; display: flex; flex-direction: column; align-items: center; justify-content: center;">'+
+    '<h3 style="margin:0 0 8px 0;color:#008bf8">' + title + '</h3>'+
     '<img src="'+ image +'" class="popup-image" />'+
-    '<p style="margin:4px 0;font-size:0.9em">'+ description +'</p></div>'
+    '<p style="margin:4px 0;font-size:1.1em">'+ description +'</p></div>'
 }
 
 
@@ -129,29 +129,30 @@ const container = document.getElementById("cards-container");
 const slider = document.querySelector('.cards-container');
 
 let isDragging = false;
-let startX = 0;
-let startScrollLeft = 0;
-let currentX = 0;
+let startY = 0;
+let startScrollTop = 0;
+let currentY = 0;
 let rafId = null;
 
 function updateScroll() {
-    const deltaX = currentX - startX;
-    slider.scrollLeft = startScrollLeft - deltaX;
+    const deltaY = currentY - startY;
+    if (deltaY != 0) debugger;
+    slider.scrollTop = startScrollTop - deltaY;
     rafId = requestAnimationFrame(updateScroll);
 }
 
 slider.addEventListener('mousedown', (e) => {
     isDragging = true;
     slider.classList.add('active');
-    startX = e.clientX;
-    startScrollLeft = slider.scrollLeft;
-    currentX = e.clientX;
+    startY = e.clientY;
+    startScrollTop = slider.scrollTop;
+    currentY = e.clientY;
     rafId = requestAnimationFrame(updateScroll);
 });
 
 slider.addEventListener('mousemove', (e) => {
     if (!isDragging) return;
-    currentX = e.clientX;
+    currentY = e.clientY;
 });
 
 slider.addEventListener('mouseup', () => {
@@ -179,14 +180,41 @@ for (const city in experiences) {
     const data = experiences[city];
     const url = staticLink.replace("%amount%", data.amount).replace("%title%", encodeURIComponent(data.title));
 
+
+    // Initializza colore card
+    let expColor = ""
+    if (data.amount <= 10){
+        expColor = "var(--tenColor)"
+    }
+    else if(data.amount <= 20){
+        expColor = "var(--twentyColor)"
+    }
+    else if(data.amount <= 50){
+        expColor = "var(--fiftyColor)"
+    }
+    else if(data.amount <= 100){
+        expColor = "var(--onehunColor)"
+    }
+    else if(data.amount <= 200){
+        expColor = "var(--twohunColor)"
+    }
+    else if(data.amount <= 300){
+        expColor = "var(--threehunColor)"
+    }
+    else if(data.amount <= 500){
+        expColor = "var(--fivehunColor)"
+    }
+
     const card = document.createElement("div");
     card.className = "card";
+    debugger;
+    card.style.borderColor = expColor;
     card.innerHTML = `
         <div class="card-details">
           <div class="text-title">${data.title}</div>
           <div class="text-body">${data.description}</div>
         </div>
-        <a class="card-button" href="#" data-city="${data.city}" data-amount="${data.amount}">${data.amount} €</a>
+        <a class="card-button" style="background-color: ${expColor}" href="#" data-city="${data.city}" data-amount="${data.amount}">${data.amount} €</a>
       `;
 
     // Aggiungi event listener per il click
@@ -206,10 +234,8 @@ for (const city in experiences) {
             data.marker.openPopup();
             
             let fixedPos = [...data.position];
-            if (isMobile){
-                // Aggiungi offset verticale per migliore visualizzazione su smartphone
-                fixedPos[0] = fixedPos[0] - 0.10;
-            }
+            // Aggiungi offset verticale per migliore visualizzazione
+            fixedPos[0] = fixedPos[0] - 0.10;
             
             map.flyTo(fixedPos, 9, {
                 animate: true, 
